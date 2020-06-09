af57683ada65aabdaaab7e8c78a159b80b572e47
vegirevathi <vegirevathi@gmail.com>
2020-06-09 16:12:46 +0530

    Added:Variables Declaration

---
+++ Gambler.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+echo "Welcome to Gambling"
+
+STAKE_PER_DAY=100
+BET_PER_GAME=1
---
+++ trail.sh
@@ -0,0 +1,6 @@
+#!/binbash -x
+
+echo "Welcome to Gambling"
+
+int STAKE_PER_DAY=100
+int BET_PER_GAME=1;

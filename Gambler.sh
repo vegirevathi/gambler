5f426cc41a8c093ad9acd87422b4dd748299016a
vegirevathi <vegirevathi@gmail.com>
2020-06-09 18:58:53 +0530

    Added:Function method to find out daily cash

--- Gambler.sh
+++ Gambler.sh
@@ -5,10 +5,26 @@ echo "Welcome to Gambling"
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 
-betResult=$((RANDOM%2))
-if [ $betResult -eq 1 ]
-then
-	echo "Won the Bet"
-else
-	echo "Lost The Bet"
-fi
+read -p "Enter the percent at which gambler can resign for the day" percent
+maxLimit=$(( $STAKE_PER_DAY + ($percent*$STAKE_PER_DAY/100) ))
+minLimit=$(( $STAKE_PER_DAY - ($percent*$STAKE_PER_DAY/100) ))
+
+function dailyCash()
+{
+	cash=$STAKE_PER_DAY
+	while [ $cash -gt $minLimit ] && [ $cash -lt $maxLimit ]
+	do
+		betResult=$((RANDOM%2))
+		if [ $betResult -eq 1 ]
+		then
+			cash=$(( $cash+$BET_PER_GAME ))
+			echo "Won the Bet"
+		else
+			cash=$(( $cash-$BET_PER_GAME ))
+			echo "Lost The Bet"
+		fi
+	done
+	echo "cash at the end of the day " $cash
+}
+dailyCash
+
